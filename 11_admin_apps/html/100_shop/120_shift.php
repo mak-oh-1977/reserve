c872dbc7166d223f5cb93e1c13568fcb03067591
makoto ohnuma <m.ohnuma@salivatech.co.jp>
2020-08-01 15:58:57 +0900

    シフト管理画面追加

--- 11_admin_apps/html/100_shop/120_shift.php
+++ 11_admin_apps/html/100_shop/120_shift.php
@@ -11,7 +11,7 @@
     <div class="container-fluid">
       <div class="row mb-2">
         <div class="col-sm-6">
-          <h1 class="m-0 text-dark">スタッフ一覧</h1>
+          <h1 class="m-0 text-dark">シフト設定</h1>
         </div><!-- /.col -->
         <div class="col-sm-6">
         </div><!-- /.col -->
